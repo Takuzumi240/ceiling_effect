02be356b39ebd97e6bea390310d015c07aa72de7
Takuzumi240 <nishio@jsk.imi.i.u-tokyo.ac.jp>
2019-06-05 18:01:21 +0900

    temp

--- scripts/hydrusx_ceil_effect_control.py
+++ scripts/hydrusx_ceil_effect_control.py
@@ -13,32 +13,38 @@ from jsk_recognition_msgs.msg import ModelCoefficientsArray
 from ceil_effect_control.msg import distance_to_ceilingwall
 from spinal.msg import FourAxisCommand
 from nav_msgs.msg import Odometry
+from spinal.msg import PwmInfo
 from spinal.msg import MotorInfo
-#from spinal.msg import Pwms
+from spinal.msg import Pwms  ############################################
 
 class ceil_effect_controller():
     def __init__(self):
 
         self.rotor_num = 4
         self.rotor_diameter = 0.355
-        self.offset = 0.043 #between cog and rotor 
+        self.offset = 0.04 #between cog and rotor 
         self.a_without_ceil = 0.0045800748
         self.b_without_ceil = -0.2564642520
-        self.alfa = 0.04
-        self.kappa = 1.2
+        self.alfa = 0.035
+        self.kappa = 1.6
         self.ceil_dist_inf = 3 #nondimensional
 
         self.ceil_dist_sub = rospy.Subscriber("/uav/cog/odom", Odometry, self.real_plane_dist)
-        self.pwm_ceil_pub = rospy.Publisher("/pwm_info",MotorInfo,queue_size=10)
+        self.pwm_ceil_pub = rospy.Publisher("/motor_info",PwmInfo,queue_size=10)
+
+        #self.pwm_sub =rospy.Subscriber("motor_pwms", Pwms, self.ceil_control_thrust)##############################
+        #self.ceil_control_pub.Publisher("aerial_robot_contril_four_axis", FourAxisCommand, queue_size = 10)##################################################
 
         #TO DO sub
     #def ceil_detect(self):
 
-        self.real_ceil_z =1.5
+        self.real_ceil_z =1.05
 
     def real_plane_dist(self, odom):
         self.real_ceil_dist = self.real_ceil_z - odom.pose.pose.position.z -self.offset
-        #print("real_ceil_dist ===", self.real_ceil_dist)
+        if (self.real_ceil_dist < 0.062):
+            self.real_ceil_dist = 0.062
+
         self.thrust_conversion_to_ceil(self.real_ceil_dist)
 
     def thrust_conversion_to_ceil(self, real_ceil_dist):
@@ -51,16 +57,23 @@ class ceil_effect_controller():
         coefficients_ceil.polynominal[2] = self.a_ceil*10
         coefficients_ceil.polynominal[3] = 0.00
         coefficients_ceil.polynominal[4] = 0.00
-        self.pwm_ceil_pub.publish(coefficients_ceil)
-        #print(coefficients_ceil)
 
+        pwm_msg = PwmInfo()
+        pwm_msg.min_thrust = 1.0
+        pwm_msg.max_thrust = 16.0
+        pwm_msg.abs_max_pwm = 0.899999976158
+        pwm_msg.force_landing_thrust = 6.5
+        pwm_msg.pwm_conversion_mode = 0
+
+        pwm_msg.motor_info.append(coefficients_ceil)
+
+        self.pwm_ceil_pub.publish(pwm_msg)
+        rospy.sleep(0.025)
 
     def thrust_ceil_coefficients(self, real_ceil_dist):
         ceil_func = 1 + self.alfa / math.pow((real_ceil_dist / self.rotor_diameter), self.kappa)
         self.a_ceil = self.a_without_ceil*ceil_func
         self.b_ceil = self.b_without_ceil*ceil_func
-
-
 
 
 if __name__ == "__main__":
@@ -70,6 +83,7 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         try:
             ceil_controller = ceil_effect_controller()
+
         except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.sleep(1)
             print"error"
--- scripts/simple_flight_control_z.py
+++ scripts/simple_flight_control_z.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python
+
 import sys
 import rospy
+import math
 from aerial_robot_msgs.msg import FlightNav
 
-rospy.init_node("simple_flight_control_z")
-nav_control_pub = rospy.Publisher("/uav/nav", FlightNav, queue_size=1)
+class ceil_approach_nav():
+    def __init__(self):
 
-rospy.sleep(1)
+        self.t = 0
+        self.dt = 0.001
+        self.z = rospy.get_param("~z", 0.5)
+        self.z_diff = rospy.get_param("~z_diff", 0.2)
+        self.curve_factor = rospy.get_param("~curve_factor", 0.01)
 
-z = rospy.get_param("~z", 1.2)
-desire_nav = FlightNav()
-
-desire_nav.pos_z_nav_mode = 2
-desire_nav.target_pos_z = z
-
-nav_control_pub.publish(desire_nav)
-
+        self.nav_control_pub = rospy.Publisher("/uav/nav", FlightNav, queue_size=1)
+        self.desire_nav = FlightNav()
 
 
+    def tar_track_sigmoid(self):
+        sigmoid = 2*((math.exp(self.curve_factor*self.t)/(math.exp(self.curve_factor*self.t) + 1)) - 0.5)
+        tar_z = self.z + self.z_diff*sigmoid
+
+        self.desire_nav.pos_z_nav_mode = 2
+        self.desire_nav.target_pos_z = tar_z
+
+        self.nav_control_pub.publish(self.desire_nav)
+        self.t = self.t + self.dt
+        print(tar_z)
+
+        return  self.z + self.z_diff - tar_z
+
+
+
+if __name__=="__main__":
+
+    rospy.init_node("simple_flight_control_z")
+    ceil_approacher = ceil_approach_nav()
+
+    while True:
+        try:
+            diff = ceil_approacher.tar_track_sigmoid()
+
+            if diff < 0.001:
+                break
+
+        except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            print (e)
 
